e226022d7986be97e4e00b8f94f9e6ba65a9cf97
Anwesha-Dutta <anweshadutta891@gmail.com>
2023-07-05 19:52:19 +0530

    Project 5 Travel Webit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Project-5-Travel-Website CUSTOM"
---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+    $('.slider').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+$('.slider2').owlCarousel({
+    loop:true,
+    margin:25,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.slider3').owlCarousel({
+    loop:true,
+    margin:25,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
